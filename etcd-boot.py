e5243ddd79157a7f51d3b587b2ececa287d1069d
Tom Taylor <tom@tommyt.co.uk>
2016-12-12 16:12:51 +0000

    Artificial delay for Amazons eventually consistent DNS

--- etcd-boot.py
+++ etcd-boot.py
@@ -3,6 +3,7 @@ import requests
 import boto3
 from sys import argv, exit
 from os import execve
+from time import sleep
 
 
 def hexify(ipv4):
@@ -169,6 +170,8 @@ if __name__ == '__main__':
 
     z.updateSRV('_etcd-server._tcp', *["0 0 2380 {}-{}.{}".format(prefix, hexify(ip), z.name) for ip in asg.ipv4s])
     z.updateSRV('_etcd-client._tcp', *["0 0 2379 {}-{}.{}".format(prefix, hexify(ip), z.name) for ip in asg.ipv4s])
+
+    sleep(10) # Artificial delay for Amazons eventually consistent DNS
 
     new_env = {
             'ETCD_ADVERTISE_CLIENT_URLS': 'http://{}:2379'.format(m.private_ipv4),
